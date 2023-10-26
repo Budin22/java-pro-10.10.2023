20cd8b5ff24835690c8e76a2fcb9ec9978b7f20b
Sergii Budin <apserbud22@gmail.com>
2023-10-26 10:58:18 +0300

    add findSymbolOccurance method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(findSymbolOccurance("Hello milk food", ' '));
     }
 
+    private static int findSymbolOccurance(String str, char myChar){
+        char[] charArray = str.toCharArray();
+        int charRepeated = 0;
+        for(char c : charArray){
+            if(c == myChar){
+                charRepeated++;
+            }
+        }
+        return charRepeated;
+    }
 }
