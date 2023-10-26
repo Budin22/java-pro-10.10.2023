c5b26aa5b6387311636c20d7e1483b2e34e69b5b
Sergii Budin <apserbud22@gmail.com>
2023-10-26 10:00:02 +0300

    add pringtColor  method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,21 @@ public class Main {
 
         printThreeWords();
         checkSumSign();
+        printColor();
 
+    }
+
+    private  static  void printColor(){
+        int value = 0;
+        if(value <= 0){
+            System.out.println("Червоний");
+            return;
+        }
+        if(value <= 100){
+            System.out.println("Жовтий");
+            return;
+        }
+        System.out.println("Зелений");
     }
 
     private static void printThreeWords(){
