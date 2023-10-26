cbb0b231c3ad2406091dc6440b42fe7ffbeff5c2
Sergii Budin <apserbud22@gmail.com>
2023-10-26 10:17:42 +0300

    add checkSumInDiapason and checkSign methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,23 @@ public class Main {
         checkSumSign();
         printColor();
         compareNumbers();
+        System.out.println("(5+9) in diapason from 10 to 20: " + checkSumInDiapason(5,9));
 
+        checkSign(-1);
     }
 
+    private static void checkSign(int a){
+        System.out.printf(a < 0 ? "%d: negative %n" : "%d: positive %n", a);
+    }
+
+    private static boolean checkSumInDiapason(int a, int b){
+        return a+b >= 10 && a+b <= 20;
+    }
 
     private static void compareNumbers(){
         int a = 8;
         int b = 12;
-        System.out.printf(a >= b ? "%d >= %d" : "%d < %d", a,b);
+        System.out.printf(a >= b ? "%d >= %d %n" : "%d < %d %n", a,b);
     }
 
     private  static  void printColor(){
