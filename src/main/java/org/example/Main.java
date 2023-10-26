89d195f25c7ac61fab8e4b7850a2994ea0e17c72
Sergii Budin <apserbud22@gmail.com>
2023-10-26 09:53:33 +0300

    add printThreeWords and checkSumSign methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,24 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        printThreeWords();
+        checkSumSign();
+
+    }
+
+    private static void printThreeWords(){
+        System.out.println("""
+                Apple
+                Orange
+                Banana
+                """);
+    }
+
+    private static void checkSumSign(){
+        int a = 8;
+        int b = 12;
+        System.out.println((a+b) >= 0 ? "Сума позитивна" : "Сума негативна");
+
     }
 
 }
