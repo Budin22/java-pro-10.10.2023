36be6efc62af501dd7265ce7d76a4881178b7861
Sergii Budin <apserbud22@gmail.com>
2023-10-26 12:13:27 +0300

    add guessWordGame method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,6 +12,37 @@ public class Main {
         System.out.println("'Hello' is palindrome: " + isPalindrome("Hello"));
         System.out.println("'Appa' is palindrome: " + isPalindrome("Appa"));
 
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado" , "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", " pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        guessWordGame(words);
+    }
+
+    private static void guessWordGame(String[] words){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("You need to guess one of this words: \n" + Arrays.toString(words));
+        System.out.println("Or write 'exit' to stop game");
+
+        String guessWord = words[(int) Math.floor(Math.random()*(words.length -1))];
+//        System.out.println("The guess word is: " + guessWord);
+
+        char[] result = new char[]{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'};
+        String userGuesses = scanner.next();
+        while (!userGuesses.equals("exit")){
+            if(guessWord.equals(userGuesses)){
+                System.out.println("You win the guess word was: " + guessWord);
+                userGuesses = "exit";
+            } else {
+                for (int i = 0; i < userGuesses.length(); i++) {
+                    if(guessWord.charAt(i) == userGuesses.charAt(i)){
+                        result[i] = guessWord.charAt(i);
+                    }
+                }
+                System.out.println(result);
+                userGuesses = scanner.next();
+            }
+        }
+        scanner.close();
     }
 
     private static boolean isPalindrome(String str){
