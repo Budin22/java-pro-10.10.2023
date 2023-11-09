56704ce25b776cd006f55f99ae52be4bee2cc225
Sergii Budin <apserbud22@gmail.com>
2023-11-09 19:37:52 +0200

    add ValueCalculator and implement task logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
-
 public class Main {
     public static void main(String[] args) {
-
+        ValueCalculator calc = new ValueCalculator(2_000_000);
+        calc.doSomeCalc();
     }
 }
