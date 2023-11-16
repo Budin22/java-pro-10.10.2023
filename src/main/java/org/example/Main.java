7417ee3de6d6ec3099da785e92a5b75bd0f3fa26
Sergii Budin <apserbud22@gmail.com>
2023-11-16 10:40:38 +0200

    add readme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,7 @@
 package org.example;
 
-import org.example.station.FuelStation;
-import org.example.station.PetrolStation;
-
 public class Main {
     public static void main(String[] args) {
-        try(FuelStation station = new PetrolStation(3200);) {
-            for (int i = 1; i < 10; i++) {
-                station.doRefuel(100 * i);
-            }
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+
     }
 }
--- src/main/java/org/example/station/FuelStation.java
+++
@@ -1,5 +0,0 @@
-package org.example.station;
-
-public interface FuelStation extends AutoCloseable {
-    void doRefuel(double fuelAmount);
-}
