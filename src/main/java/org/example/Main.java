53c3f7e8e2075ed6c70a179be1be0dfe98f4279d
Sergii <apserbud22@gmail.com>
2023-11-27 12:33:31 +0200

    add CoffeeOrderBoard, some demo to Main and tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.coffe.order.CoffeeOrderBoard;
 
 public class Main {
     public static void main(String[] args) {
+        CoffeeOrderBoard orderBoard = new CoffeeOrderBoard();
+
+        orderBoard.deliver();
+        for (int i = 1; i < 15; i++) {
+            orderBoard.addOrder("order#" + i);
+        }
+        System.out.println("Get order in line: " + orderBoard.deliver());
+        System.out.println("Get order by number 4: " + orderBoard.deliver(4));
+        System.out.println("Get order by number 5: " + orderBoard.deliver(5));
+        System.out.println("Get order by number 5: " + orderBoard.deliver(5));
+        System.out.println("Get order by number 12: " + orderBoard.deliver(12));
+        System.out.println("Get order by number 1: " + orderBoard.deliver(1));
+        System.out.println("Get order by number 14: " + orderBoard.deliver(14));
+        System.out.println("Get order in line: " + orderBoard.deliver());
+        System.out.println("Get order in line: " + orderBoard.deliver());
+
+        orderBoard.printOrdersInLine();
     }
 }
--- src/main/java/org/example/coffe/order/CoffeeOrderBoard.java
+++ src/main/java/org/example/coffe/order/CoffeeOrderBoard.java
@@ -1,4 +1,50 @@
 package org.example.coffe.order;
 
-public class CoffeeOrderBoard {
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoffeeOrderBoard implements OrderBoard<Order> {
+    private int lastOrder = 0;
+    private int firstOrder = 1;
+    private final Map<Integer, Order> orders;
+    public CoffeeOrderBoard(){
+        orders = new HashMap<>();
+    }
+
+    public void addOrder(String name) {
+        lastOrder++;
+        Order order = new Order(lastOrder, name);
+        orders.put(lastOrder, order);
+    }
+
+    public Order deliver() {
+        if (lastOrder == 0) {
+            System.out.println("We don't have any orders yet");
+            return null;
+        }
+        Order order = orders.remove(firstOrder);
+        while (order == null && firstOrder < lastOrder) {
+            firstOrder++;
+            order = orders.remove(firstOrder);
+        }
+        return order;
+    }
+
+    public Order deliver(int num) {
+        Order order = orders.remove(num);
+        if (order != null) {
+            return order;
+        } else {
+            System.out.println("We don't have order with num: " + num);
+            return null;
+        }
+    }
+
+    public void printOrdersInLine() {
+        System.out.println(orders.values());
+    }
+
+    public Map<Integer,Order> getOrders(){
+        return orders;
+    }
 }
