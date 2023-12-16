0f27de34f1e2440b085812650f57bce8d4df68ca
Sergii <apserbud22@gmail.com>
2023-12-16 14:42:56 +0200

    add readme

--- src/main/java/org/example/exception/NoSuchElementException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class NoSuchElementException extends RuntimeException {
-    public NoSuchElementException(String message) {
-        super("Product with category " + message + " not found");
-    }
-}
