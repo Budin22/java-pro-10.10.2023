df588f1365a0f5cd5a02efd79e292ce93a775630
Sergii <apserbud22@gmail.com>
2023-12-18 12:18:42 +0200

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
-import org.example.test.TestedClass;
-import org.example.test.TestRunner;
-
 public class Main {
     public static void main(String[] args) {
-        TestRunner testRunner = new TestRunner();
-        testRunner.start(TestedClass.class);
     }
 }
