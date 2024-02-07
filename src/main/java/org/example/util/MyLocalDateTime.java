e54555ecd34669084aa45e2cbdc0846c6f33d0c8
Sergii <apserbud22@gmail.com>
2024-02-07 15:35:43 +0200

    add MyLocalDateTime

--- src/main/java/org/example/util/MyLocalDateTime.java
+++ src/main/java/org/example/util/MyLocalDateTime.java
@@ -1,2 +1,12 @@
-package org.example.util;public class MyLocalDateTime {
+package org.example.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MyLocalDateTime {
+    private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static LocalDateTime getLocalDateTimeFromString(String timestamp) {
+        return LocalDateTime.parse(timestamp, FORMATTER);
+    }
 }
