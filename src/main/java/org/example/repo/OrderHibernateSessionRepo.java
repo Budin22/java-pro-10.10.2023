c29713877573f8b41f60af1adbbfff1e42a7d791
Sergii <apserbud22@gmail.com>
2024-02-14 18:41:56 +0200

    add Order and Product entity

---
+++ src/main/java/org/example/repo/OrderHibernateSessionRepo.java
@@ -0,0 +1,22 @@
+package org.example.repo;
+
+import org.example.model.Order;
+
+import java.util.List;
+
+public class OrderHibernateSessionRepo implements OrderRepo{
+    @Override
+    public Order getOrderById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public Order addOrder(Order order) {
+        return null;
+    }
+}
