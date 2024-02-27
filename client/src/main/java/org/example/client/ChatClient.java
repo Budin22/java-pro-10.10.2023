11a3d201980449f66a1775480b65c2576aa552aa
Sergii <apserbud22@gmail.com>
2024-02-27 13:52:26 +0200

    add FileSenderTest and test.txt

--- client/src/main/java/org/example/client/ChatClient.java
+++ client/src/main/java/org/example/client/ChatClient.java
@@ -13,39 +13,42 @@ public class ChatClient implements AutoCloseable {
 
     public void connect() {
         try {
-            final InputStream inputStream = socket.getInputStream();
-            final OutputStream outputStream = socket.getOutputStream();
-
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             boolean[] isOnChat = {true};
-
-            Thread receiveMessage = new Thread(() ->
-            {
-                while (isOnChat[0]) {
-                    Scanner scanner = new Scanner(System.in);
-                    String userMessage = scanner.nextLine();
-                    if (userMessage.equals("STOP")) {
-                        isOnChat[0] = false;
-                    }
-
-                    writer.println(userMessage);
-                    writer.flush();
-                }
-            }
-            );
-            receiveMessage.start();
-
-            while (isOnChat[0]) {
-                String s = reader.readLine();
-                System.out.println(s);
-            }
+            writeMessage(socket, isOnChat);
+            readMessage(socket, isOnChat);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
+    private void writeMessage(Socket socket, boolean[] isOnChat) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
+        new Thread(() -> {
+            while (isOnChat[0]) {
+                Scanner scanner = new Scanner(System.in);
+                String userMessage = scanner.nextLine();
+
+                writer.println(userMessage);
+                writer.flush();
+                if (userMessage.equals("exit")) {
+                    isOnChat[0] = false;
+                }
+            }
+        }
+        ).start();
+    }
+
+    private void readMessage(Socket socket, boolean[] isOnChat) throws IOException {
+        final InputStream inputStream = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        while (isOnChat[0]) {
+            String s = reader.readLine();
+            if (s != null) System.out.println(s);
+        }
+    }
 
     @Override
     public void close() throws Exception {
---
+++ server/src/test/java/org/example/sender/FileSenderTest.java
@@ -0,0 +1,2 @@
+package org.example.sender;public class FileSenderTest {
+}
